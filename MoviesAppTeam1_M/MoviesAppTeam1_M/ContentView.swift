9c960f42d17eacc6b8bf4329fd346f61b2b9a7bf
wasan <twasanj@gmail.com>
2025-12-23 10:33:21 +0300

    FIRST

---
+++ MoviesAppTeam1_M/MoviesAppTeam1_M/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoviesAppTeam1_M
+//
+//  Created by wasan jayid althagafi on 03/07/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesAppTeam1_M/MoviesAppTeam1_M/MoviesAppTeam1_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppTeam1_MApp.swift
+//  MoviesAppTeam1_M
+//
+//  Created by wasan jayid althagafi on 03/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppTeam1_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
